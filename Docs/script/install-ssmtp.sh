d6e17f35b91d6d1e28486d1eb38d034b1c02c0c9
huydv <61343215+huydv398@users.noreply.github.com>
2021-08-06 11:45:57 +0700

    Update install-ssmtp.sh

--- Docs/script/install-ssmtp.sh
+++ Docs/script/install-ssmtp.sh
@@ -3,36 +3,39 @@
 # Check OS
 cat /etc/os-release* |grep 'ubuntu' > /dev/null 2>&1 && OS='Ubuntu'
 cat /etc/os-release* |grep 'centos' > /dev/null 2>&1 && OS='CentOS' 
-
+if [ -e /usr/sbin/ssmtp ]
+then
+    sta=1
+    echo "SSMTP is installed" 
+else
+    sta=0
+    
+fi
 # install_mariadb
 f_install_ssmtp(){
-    if [ -e /usr/sbin/ssmtp ]
+    if [ "$OS" == "CentOS" ]
     then
-        sta=1
-        echo "SSMTP is installed" 
-    else
-        sta=0
-        if [ "$OS" == "CentOS" ]
-        then
-            echo "Install SSMTP for: "$OS
-            yum install epel-release -y
-            yum update -y
-            yum install -y ssmtp
+        echo "Install SSMTP for: "$OS
+        yum install epel-release -y
+        yum update -y
+        yum install -y ssmtp
 
-        elif [ "$OS" == "Ubuntu" ]
-        then
-            echo $OS
-            apt-get update
-            apt install ssmtp -y
-        else
-            echo "OS none supported"
-        fi
+    elif [ "$OS" == "Ubuntu" ]
+    then
+        echo $OS
+        apt-get update
+        apt install ssmtp -y
+    else
+        echo "OS none supported"
     fi
     
 }
 f_conf_ssmtp(){
-    cp /etc/ssmtp/ssmtp.conf /etc/ssmtp/ssmtp-q.conf.bak
-
+    cp /etc/ssmtp/ssmtp.conf /etc/ssmtp/ssmtp.conf.bak
+    read -p "SMTP server:(Enter for: smtp.gmail.com): " smtpserver
+    read -p "SMTP Username (Ex: user@gmail.com): " smtpuser
+    read -p "SMTP Password (password for $smtpuser): " smtppassword
+    read -p "SMTP port:(Enter for: 587): " smtpport
     if [ $smtpserver="" ]
     then 
         smtpserver=smtp.gmail.com
@@ -59,12 +62,6 @@ TLS_CA_File=/etc/pki/tls/certs/ca-bundle.crt
     " >> /etc/ssmtp/revaliases
     sleep 5
     
-}
-f_in_info_main(){
-    read -p "SMTP server:(Enter for: smtp.gmail.com): " smtpserver
-    read -p "SMTP Username (Ex: user@gmail.com): " smtpuser
-    read -p "SMTP Password (password for $smtpuser): " smtppassword
-    read -p "SMTP port:(Enter for: 587): " smtpport
 }
 main(){
     set -e
